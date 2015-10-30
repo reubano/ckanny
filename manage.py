94f1cb970ff1c95c16665831e2ffcfbeb1b57947
Reuben Cummings <reubano@gmail.com>
2015-10-30 13:02:13 +0300

    Refactor testing

--- manage.py
+++ manage.py
@@ -47,10 +47,15 @@ def require():
     call(cmd, shell=True)
 
 
+@manager.arg('where', 'w', help='test path', default=None)
+@manager.arg(
+    'stop', 'x', help='Stop after first error', type=bool, default=False)
 @manager.command
-def test():
+def test(where=None, stop=False):
     """Run nose and script tests"""
-    call('python %s' % p.join(_basedir, 'tests', 'test_script.py'), shell=True)
+    opts = '-xv' if stop else '-v'
+    opts += 'w %s' % where if where else ''
+    call([p.join(_basedir, 'helpers', 'test'), opts])
 
 
 @manager.command
